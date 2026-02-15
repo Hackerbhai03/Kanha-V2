a23a7618a912c58922b844849e8134179b16af9b
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-02-15 18:09:23 +0530

    ᴜᴘʟᴏᴀᴅᴇᴅ ᴠɪᴀ ɢɪᴛʜᴜʙ ᴜᴘʟᴏᴀᴅᴇʀ ʙᴏᴛ

---
+++ KanhaMusic/utils/inline/__init__.py
@@ -0,0 +1,22 @@
+# -----------------------------------------------
+# 🔸 KanhaMusic Project
+# 🔹 Developed & Maintained by: Kanha Bots (https://github.com/TEAM-Kanha-OP)
+# 📅 Copyright © 2025 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by TEAM-Kanha-OP
+# -----------------------------------------------
+
+
+from .extras import *
+from .help import *
+from .play import *
+from .queue import *
+from .settings import *
+from .speed import *
+from .start import *
