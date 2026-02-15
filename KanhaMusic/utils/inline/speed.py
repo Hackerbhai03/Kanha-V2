22c49f876977d8e8e97e5671d5af92a30a254b3a
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-02-15 18:09:15 +0530

    ᴜᴘʟᴏᴀᴅᴇᴅ ᴠɪᴀ ɢɪᴛʜᴜʙ ᴜᴘʟᴏᴀᴅᴇʀ ʙᴏᴛ

---
+++ KanhaMusic/utils/inline/speed.py
@@ -0,0 +1,55 @@
+# -----------------------------------------------
+# 🔸 KanhaMusic Project
+# 🔹 Developed & Maintained by: Kanha Bots (https://github.com/TEAM-Kanha-OP)
+# 📅 Copyright © 2025 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by TEAM-Kanha-OP
+# -----------------------------------------------
+
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def speed_markup(_, chat_id):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text="🕒 0.5x",
+                    callback_data=f"SpeedUP {chat_id}|0.5",
+                ),
+                InlineKeyboardButton(
+                    text="🕓 0.75x",
+                    callback_data=f"SpeedUP {chat_id}|0.75",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["P_B_4"],
+                    callback_data=f"SpeedUP {chat_id}|1.0",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="🕤 1.5x",
+                    callback_data=f"SpeedUP {chat_id}|1.5",
+                ),
+                InlineKeyboardButton(
+                    text="🕛 2.0x",
+                    callback_data=f"SpeedUP {chat_id}|2.0",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"],
+                    callback_data="close",
+                ),
+            ],
+        ]
+    )
+    return upl
