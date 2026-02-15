e61682fbe06c66bc3498f2a136a13e06e43a3ad8
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-02-15 18:07:50 +0530

    ᴜᴘʟᴏᴀᴅᴇᴅ ᴠɪᴀ ɢɪᴛʜᴜʙ ᴜᴘʟᴏᴀᴅᴇʀ ʙᴏᴛ

---
+++ KanhaMusic/plugins/admins/stop.py
@@ -0,0 +1,37 @@
+# -----------------------------------------------
+# 🔸 KanhaMusic Project
+# 🔹 Developed & Maintained by: Kanha Bots (https://github.com/TEAM-Kanha-OP)
+# 📅 Copyright © 2025 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by TEAM-Kanha-OP
+# -----------------------------------------------
+
+
+from pyrogram import filters
+from pyrogram.types import Message
+from KanhaMusic import app
+from KanhaMusic.core.call import Kanha
+from KanhaMusic.utils.database import set_loop
+from KanhaMusic.utils.decorators import AdminRightsCheck
+from KanhaMusic.utils.inline import close_markup
+from config import BANNED_USERS
+
+
+@app.on_message(
+    filters.command(["end", "stop", "cend", "cstop"]) & filters.group & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return
+    await Kanha.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    await message.reply_text(
+        _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
+    )
