12f9f4e56778f1f0545001ebddbc8f5b083c3b02
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-02-15 18:07:10 +0530

    ᴜᴘʟᴏᴀᴅᴇᴅ ᴠɪᴀ ɢɪᴛʜᴜʙ ᴜᴘʟᴏᴀᴅᴇʀ ʙᴏᴛ

---
+++ KanhaMusic/platforms/__init__.py
@@ -0,0 +1,22 @@
+# -----------------------------------------------
+# 🔸 KanhaMusic Project
+# 🔹 Developed & Maintained by: Kanha Bots (https://github.com/TEAM-Kanha-OP)
+# 📅 Copyright © 2025 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by TEAM-Kanha-OP
+# -----------------------------------------------
+
+
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
