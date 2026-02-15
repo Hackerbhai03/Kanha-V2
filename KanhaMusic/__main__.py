2d35be09878de82fbd0db13c3170f2e23d956f21
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-02-15 18:07:05 +0530

    ᴜᴘʟᴏᴀᴅᴇᴅ ᴠɪᴀ ɢɪᴛʜᴜʙ ᴜᴘʟᴏᴀᴅᴇʀ ʙᴏᴛ

---
+++ KanhaMusic/__main__.py
@@ -0,0 +1,74 @@
+# -----------------------------------------------
+# 🔸 KanhaMusic Project
+# 🔹 Developed & Maintained by: Kanha Bots (https://github.com/TEAM-Kanha-OP)
+# 📅 Copyright © 2025 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by TEAM-Kanha-OP
+# -----------------------------------------------
+
+import asyncio
+import importlib
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+import config
+from KanhaMusic import LOGGER, app, userbot
+from KanhaMusic.core.call import Kanha
+from KanhaMusic.misc import sudo
+from KanhaMusic.plugins import ALL_MODULES
+from KanhaMusic.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
+        exit()
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("KanhaMusic.plugins" + all_module)
+    LOGGER("KanhaMusic.plugins").info("sᴜᴄᴄᴇssғᴜʟʟʏ ɪᴍᴘᴏʀᴛᴇᴅ ᴀʟʟ ᴍᴏᴅᴜʟᴇs...")
+    await userbot.start()
+    await Kanha.start()
+    try:
+        await Kanha.stream_call("https://te.legra.ph/file/39b302c93da5c457a87e3.mp4")
+    except NoActiveGroupCall:
+        LOGGER("KanhaMusic").error(
+            "ʙsᴅᴋ ᴠᴄ ᴛᴏ ᴏɴ ᴋᴀʀʟᴇ  ʟᴏɢ ɢʀᴏᴜᴘ\ᴄʜᴀɴɴᴇʟ ᴋɪ.\n\n ᴏɴ ᴋᴀʀᴋᴇ ᴀᴀ ᴛᴀʙ ᴛᴀᴋ ʙᴏᴛ ʙᴀɴᴅ ᴋᴀʀ ʀʜᴀ ʜᴏᴏɴ..."
+        )
+        exit()
+    except:
+        pass
+    await Kanha.decorators()
+    LOGGER("KanhaMusic").info(
+        "ᴍᴜsɪᴄ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ, ɴᴏᴡ ɢɪʙ ʏᴏᴜʀ ɢɪʀʟғʀɪᴇɴᴅ ᴄʜᴜᴛ ɪɴ @KanhaBots"
+    )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("KanhaMusic").info("ᴍᴀᴀ ᴄʜᴜᴅᴀ ᴍᴀɪɴ ʙᴏᴛ ʙᴀɴᴅ ᴋᴀʀ ʀʜᴀ Aᴀʀᴜᴍɪ Mᴜsɪᴄ Bᴏᴛ...")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
