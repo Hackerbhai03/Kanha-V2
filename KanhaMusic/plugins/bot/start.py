c73fdace7a089f920c3c97d0f553c8cd8f1610f4
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-02-27 17:31:24 +0530

    Update start.py

--- KanhaMusic/plugins/bot/start.py
+++ KanhaMusic/plugins/bot/start.py
@@ -1,6 +1,6 @@
 import random
 import time
-
+import asyncio
 from youtubesearchpython.__future__ import VideosSearch
 from pyrogram import filters
 from pyrogram.enums import ChatType
@@ -14,8 +14,8 @@ from KanhaMusic.utils.database import (add_served_chat, add_served_user,
                                        blacklisted_chats, get_lang,
                                        is_banned_user, is_on_off)
 from KanhaMusic.utils.decorators.language import LanguageStart
-from KanhaMusic.utils.formatters import get_readable_time
-from KanhaMusic.utils.inline import help_pannel, private_panel, start_panel
+from KanhaMusicy.utils.formatters import get_readable_time
+from KanhaMusic.utils.inline import first_page, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
@@ -24,12 +24,7 @@ EFFECT_ID = [
     5104841245755180586,
     5107584321108051014,
     5107584321108051014,
-    5104841245755180586,
-    5107584321108051014,
-    5104841245755180586,
-    5107584321108051014,
 ]
-
 
 START_IMAGES = [
     "https://files.catbox.moe/2bia7i.jpg",
@@ -38,19 +33,29 @@ START_IMAGES = [
     "https://files.catbox.moe/cudd9u.jpg"
 ]
 
+async def change_img():
+    global START_IMAGES
+    while True:
+        await asyncio.sleep(5)
+        random.shuffle(START_IMAGES)
 
+async def start_task():
+    asyncio.create_task(change_img())
 
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
-    await message.react("❤️")
+    await message.react("🍓")
+    
+    random_image = random.choice(START_IMAGES)
+    
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
-            keyboard = help_pannel(_)
+            keyboard = first_page(_)
             return await message.reply_photo(
-                photo=config.START_IMG_URL,
+                photo=random_image,
                 has_spoiler=True,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
@@ -104,7 +109,7 @@ async def start_pm(client, message: Message, _):
     else:
         out = private_panel(_)
         await message.reply_photo(
-            photo=config.START_IMG_URL,
+            photo=random_image,
             has_spoiler=True,
             message_effect_id=random.choice(EFFECT_ID),
             caption=_["start_2"].format(message.from_user.mention, app.mention),
@@ -116,20 +121,19 @@ async def start_pm(client, message: Message, _):
                 text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
             )
 
-
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
+    random_image = random.choice(START_IMAGES)
     await message.reply_photo(
-        photo=config.START_IMG_URL,
+        photo=random_image,
         has_spoiler=True,
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
     return await add_served_chat(message.chat.id)
-
 
 @app.on_message(filters.new_chat_members, group=-1)
 async def welcome(client, message: Message):
@@ -158,8 +162,9 @@ async def welcome(client, message: Message):
                     return await app.leave_chat(message.chat.id)
 
                 out = start_panel(_)
+                random_image = random.choice(START_IMAGES)
                 await message.reply_photo(
-                    photo=config.START_IMG_URL,
+                    photo=random_image,
                     has_spoiler=True,
                     caption=_["start_3"].format(
                         message.from_user.first_name,
@@ -173,3 +178,4 @@ async def welcome(client, message: Message):
                 await message.stop_propagation()
         except Exception as ex:
             print(ex)
+
