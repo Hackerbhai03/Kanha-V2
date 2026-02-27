54377cb2f3ab907b62d8350649f0eb64de5fae86
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-02-27 17:26:16 +0530

    Update thumbnails.py

--- KanhaMusic/utils/thumbnails.py
+++ KanhaMusic/utils/thumbnails.py
@@ -2,11 +2,19 @@ import random
 
 # 🔥 Tumhare diye hue images
 RANDOM_THUMBS = [
-    "https://files.catbox.moe/ikxb96.jpg",
-    "https://files.catbox.moe/a6hzmb.jpg",
-    "https://files.catbox.moe/ohezme.jpg",
-    "https://files.catbox.moe/spylio.jpg",
-    "https://files.catbox.moe/5go4t6.jpg",
+    
+    "https://files.catbox.moe/vbdda6.jpg",
+    "https://files.catbox.moe/3up9ky.jpg",
+    "https://files.catbox.moe/jktiak.jpg",
+    "https://files.catbox.moe/0n4439.jpg",
+    "https://files.catbox.moe/l2id2z.jpg",
+    "https://files.catbox.moe/l2id2z.jpg",
+    "https://files.catbox.moe/8c6zfn.jpg",
+    "https://files.catbox.moe/to3v10.jpg",
+    "https://files.catbox.moe/mcqu0j.jpg",
+    "https://files.catbox.moe/2803m5.jpg",
+    "https://files.catbox.moe/gf3142.jpg",
+    "https://files.catbox.moe/gcqh0j.jpg"
     
 ]
 
